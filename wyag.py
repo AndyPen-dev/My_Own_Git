75670179349fa8ce41e61c3a833b384ef959d051
AndyPen-dev <andypen123@yahoo.com>
2023-07-15 21:13:07 -0400

    test

--- wyag.py
+++ wyag.py
@@ -10,4 +10,16 @@ import zlib  #for git compress
 from math import ceil 
 
 
+parser = argparse.ArgumentParser(description="homemade git")
+
+#######HANDLING GIT SUB COMMANDS#########
+subparser = parser.add_subparsers(title="Commands", dest="command")
+subparser.required = True
+
+def main(argv=sys.argv[1:]):
+    args = parser.parse_args(argv)
+    
+    if args.command == "add"    :cmd_add(args)
+    print("Hello")
+
 
